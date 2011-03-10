13ad6794c91ba96119098a7da0ecbb5351433b5f
Michael Herold <michael.j.herold@gmail.com>
2011-03-10 15:30:13 -0500

    Removed TSLPC from the YPlugin.java base file. Per http://forums.bukkit.org/threads/oops-i-broke-your-plugins.599/#post-70677

--- files/YPlugin.java
+++ files/YPlugin.java
@@ -21,15 +21,6 @@ public class <pluginname> extends JavaPlugin {
     private final <pluginname>BlockListener blockListener = new <pluginname>BlockListener(this);
     private final HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>();
 
-    public <pluginname>(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader) {
-        super(pluginLoader, instance, desc, folder, plugin, cLoader);
-        // TODO: Place any custom initialisation code here
-
-        // NOTE: Event registration should be done in onEnable not here as all events are unregistered when a plugin is disabled
-    }
-
-   
-
     public void onEnable() {
         // TODO: Place any custom enable code here including the registration of any events
 
